c6e51b179419007b5343e8ae0472e547405c7aea
Ankit Jain <anku.171co208@nitk.edu.in>
2021-03-06 20:29:51 +0530

    anomalous nodes marked red in both shrinked n original graph

--- gae/anomaly_detection.py
+++ gae/anomaly_detection.py
@@ -138,19 +138,55 @@ class AnomalyDetectionRunner():
                     G.add_edge(i,j)
         return G
 
+    def savePredictedGraph(self, adj, anomalous_nodes, filename):
+        adj = adj.toarray()
+        (r,c) = adj.shape
+        if filename == 'shrink':
+            G = nx.Graph(adj)
+            color_map = []
+            t = ()
+            for i in G:
+                if i in anomalous_nodes:
+                    color_map.append('red')
+                else:
+                    color_map.append('blue')
+            node_sizes = []
+            num_nodes = r
+            for i in range(num_nodes) :
+                node_sizes.append(0.4)
+            plt.clf()
+            pos=nx.spring_layout(G)
+            nx.draw(G, pos, node_size = node_sizes, node_color=color_map, width = 0.01, with_labels=False)
+            plt.savefig("markedShrinked.png", dpi=1000, facecolor='w', edgecolor='w',orientation='portrait', papertype=None, format=None,transparent=False, bbox_inches=None, pad_inches=0.1)
+        
+        elif filename == 'original':
+            G = nx.Graph(adj)
+            color_map = []
+            t = ()
+            for i in G:
+                if i in anomalous_nodes:
+                    color_map.append('red')
+                else:
+                    color_map.append('blue')
+            node_sizes = []
+            num_nodes = r
+            for i in range(num_nodes) :
+                node_sizes.append(0.2)
+            plt.clf()
+            pos=nx.spring_layout(G)
+            nx.draw(G, pos, node_size = node_sizes, node_color=color_map, width = 0.01, with_labels=False)
+            plt.savefig("markedOriginal.png", dpi=1000, facecolor='w', edgecolor='w',orientation='portrait', papertype=None, format=None,transparent=False, bbox_inches=None, pad_inches=0.1)
+
+
     def printPredictedSubgraphs(self, adj, partition_to_node, predicted_subgraphs):
-        print(partition_to_node, predicted_subgraphs)
         for i in predicted_subgraphs:
             node_sizes = []
             for j in range(len(partition_to_node[i])) :
                 node_sizes.append(0.1)
-            print("graph number",i)
-            print("nodes in graph", partition_to_node[i])
             plt.clf()
             G = self.getAdjSubgraph(adj,partition_to_node, i)
             pos=nx.spring_layout(G)
-            # print("nodes in subgraph", i, G.number_of_nodes())
-            nx.draw(G,pos,node_size = node_sizes, node_color='#A0CBE2',edge_color='#BB0000',width=0.1,edge_cmap=plt.cm.Blues,with_labels=False)
+            nx.draw(G,pos,node_size = node_sizes,edge_color='#BB0000',width=0.1,edge_cmap=plt.cm.Blues,with_labels=False)
             plt.savefig("output/predictedSubGraphs/" + str(i) + ".png", dpi=1000, facecolor='w', edgecolor='w',orientation='portrait', papertype=None, format=None,transparent=False, bbox_inches=None, pad_inches=0.1)
             
 
@@ -165,7 +201,6 @@ class AnomalyDetectionRunner():
             plt.clf()
             G = self.getAdjSubgraph(adj,partition_to_node, i)
             pos=nx.spring_layout(G)
-            print("nodes in subgraph", i, G.number_of_nodes(), len(partition_to_node[i]))
             # nx.draw(G,pos,node_size = node_sizes, node_color='#A0CBE2',edge_color='#BB0000',width=0.1,edge_cmap=plt.cm.Blues,with_labels=False)
             # plt.savefig("output/subGraphs/" + str(i) + ".png", dpi=1000, facecolor='w', edgecolor='w',orientation='portrait', papertype=None, format=None,transparent=False, bbox_inches=None, pad_inches=0.1)
 
@@ -209,7 +244,6 @@ class AnomalyDetectionRunner():
         partition, new_adj, new_features, new_labels = self.getCoarsenedGraph(embeddings)
         new_feas =  format_data(self.data_name, new_adj, new_features, new_labels)
 
-        # print("p",partition)
         # self.saveGraphs(new_adj, "shrink")
 
         partition_to_node = dict()
@@ -218,7 +252,6 @@ class AnomalyDetectionRunner():
                 partition_to_node[partnum]=[]
             partition_to_node[partnum].append(node)
 
-        # print("p2n", partition_to_node.keys())
 
         
         # self.saveSubGraphs(feas['adj'], partition_to_node, new_adj.shape[0])
@@ -256,15 +289,20 @@ class AnomalyDetectionRunner():
                 num_count = num_count + 1
                 predicted_subgraphs.append(partition_to_node[i])
 
+    
         print("nodes in shrinked graphs",new_adj.shape[0])
         print("number of subgraphs predicted as anomalous: ", len(predicted_subgraphs))
         print("Anomalous subgraphs: ", predicted_subgraphs, num_count)
         # self.printPredictedSubgraphs(feas['adj'], partition_to_node, anomalous_shrinked_nodes)
 
+        self.savePredictedGraph(new_adj, anomalous_shrinked_nodes, "shrink")
+
         predicted_node_numbers = []
 
         for p in predicted_subgraphs:
             predicted_node_numbers+=p
+
+        self.savePredictedGraph(feas['adj'], predicted_node_numbers, "original")
 
         print(len(set(predicted_node_numbers)))
 
